43893c3671d1bcdf2154d7d34503e7037f2c6e7a
d-s-i <briacgoujon95@gmail.com>
2022-01-06 14:13:03 +0100

    update tests

--- test/00_econNFT721.test.ts
+++ test/00_econNFT721.test.ts
@@ -23,63 +23,63 @@ let attacker: SignerWithAddress;
 const displayAddresses = false;
 const displayAccounts = false;
 
-// before("EconNFT", async function() {
+before("EconNFT", async function() {
 
-//     [deployer, daoTreasury, minter, attacker] = await ethers.getSigners();
+    [deployer, daoTreasury, minter, attacker] = await ethers.getSigners();
 
-//     displayAccounts && displayAddress("deployer", deployer.address);
-//     displayAccounts && displayAddress("daoTreasury", daoTreasury.address);
-//     displayAccounts && displayAddress("minter", minter.address);
-//     displayAccounts && displayAddress("attacker", attacker.address);
+    displayAccounts && displayAddress("deployer", deployer.address);
+    displayAccounts && displayAddress("daoTreasury", daoTreasury.address);
+    displayAccounts && displayAddress("minter", minter.address);
+    displayAccounts && displayAddress("attacker", attacker.address);
 
-//     econNFT = await deployEconNft721(6);
-//     [usdc, weth] = await deployTokens();
+    econNFT = await deployEconNft721(6);
+    [usdc, weth] = await deployTokens();
 
-//     await econNFT.setMinter(minter.address);
+    await econNFT.setMinter(minter.address);
 
-//     await mintErc721Tokens(20, econNFT, minter);
+    await mintErc721Tokens(20, econNFT, minter);
 
-// });
+});
 
 describe("EconNFT", function() {
     
-    // it("Deployed the nft correctly", async function() {
+    it("Deployed the nft correctly", async function() {
     
-    //     displayAddresses && displayAddress("EconNFT", econNFT.address);
+        displayAddresses && displayAddress("EconNFT", econNFT.address);
         
-    //     assertAddressOk(econNFT.address);
-    // });
+        assertAddressOk(econNFT.address);
+    });
 
-    // it("Deployed tokens correctly", async function() {
+    it("Deployed tokens correctly", async function() {
 
-    //     displayAddresses && displayAddress("weth", weth.address);
-    //     displayAddresses && displayAddress("usdc", usdc.address);
+        displayAddresses && displayAddress("weth", weth.address);
+        displayAddresses && displayAddress("usdc", usdc.address);
 
-    //     assertAddressOk(weth.address);
-    //     assertAddressOk(usdc.address);
+        assertAddressOk(weth.address);
+        assertAddressOk(usdc.address);
 
-    // });
+    });
 
-    // it("Set Minter correctly", async function() {
+    it("Set Minter correctly", async function() {
 
-    //     const econMinter = await econNFT.minter();
+        const econMinter = await econNFT.minter();
 
-    //     assert.equal(econMinter, minter.address);
-    // });
+        assert.equal(econMinter, minter.address);
+    });
 
-    // it("Mint all 20 tokens", async function() {
+    it("Mint all 20 tokens", async function() {
 
-    //     const nftBalance = await econNFT.balanceOf(minter.address);
+        const nftBalance = await econNFT.balanceOf(minter.address);
 
-    //     assert.ok(nftBalance.eq(20));
-    // });
+        assert.ok(nftBalance.eq(20));
+    });
 
-    // it("Can't mint if isn't the minter", async function() {
-    //     try {
-    //         await mintErc721Tokens(1, econNFT, attacker);
-    //         assert.ok(false);
-    //     } catch(error) {
-    //         assert.ok(true);
-    //     }
-    // });
+    it("Can't mint if isn't the minter", async function() {
+        try {
+            await mintErc721Tokens(1, econNFT, attacker);
+            assert.ok(false);
+        } catch(error) {
+            assert.ok(true);
+        }
+    });
 });
